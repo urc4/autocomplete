f3c3c33824bc51b9ce12f50b040c2b221da1d111
othon_ishiyi <othon.ishiyi@gmail.com>
2023-10-28 17:39:16 -0300

    Comment state.h

--- src/state.h
+++ src/state.h
@@ -5,18 +5,21 @@
 #ifndef STATE_H
 #define STATE_H
 
+// Class to represent the state of the FST
 class state{
 public:
     state();
-    bool get_final();
-    void set_final(bool is_final);
-    state* get_transition(char input);
-    void set_transition(char input, state *next, int output = 0);
-    int get_output(char input);
-    void set_output(char input, int output);
+    bool get_final(); //returns true if state is final state
+    void set_final(bool is_final); //set true if final state
+    state* get_transition(char input); //returns a pointer to the state corresponding to input char
+    void set_transition(char input, state *next, int output = 0); //set transition state and value (optionally)
+    int get_output(char input); //returns the transition value
+    void set_output(char input, int output); //set the value for a transition
 
 private:
+    // for each char, stores the corresponding transition state and value
     std::unordered_map<char, std::pair<state*, int>> transitions;
+
     bool final;
 };
 
