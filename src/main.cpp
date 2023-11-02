f9bc4101f2e10d81672f17eda18fdd3582336775
othon_ishiyi <othon.ishiyi@gmail.com>
2023-11-02 11:08:36 -0300

    main.cpp implemented

--- src/main.cpp
+++ src/main.cpp
@@ -1,16 +1,14 @@
-#include "../lib/state.h"
-#include <iostream>
+#include "../lib/minimal_fst.h"
 
-//Teste
 int main() {
-    state A;
-    state *B = new state();
-    state *C = new state();
-    A.set_transition('a', B);
-    B->set_transition('a', C, 3);
-    A.set_output('a', 2);
-    A.set_final(true);
-
-    std::cout << A.get_output('a') << " " << A.get_transition('a')->get_output('a');
+    minimal_fst fst("../data/american-english-dict-ascii");
+    std::cout << "Digite a sua busca\n";
+    std::string input;
+    while (true) {
+        std::getline(std::cin, input);
+        std::cout << "\n";
+        fst.display(input, 10);
+        std::cout << "-----------------------\nDigite a sua busca\n";
+    }
     return 0;
 }
